b0e422bc39deef739460b6d3c6721bdb1d0bbb39
of654 <liuyangguang@ofcard.com>
2014-05-06 18:06:49 +0800

    add some content

---
+++ christ-core/src/main/java/org/youngguang/app/service/BaseService.java
@@ -0,0 +1,10 @@
+package org.youngguang.app.service;
+
+/**
+ * Description
+ *
+ * @author youngguang of654
+ * @date 2014/5/6 0006
+ */
+public class BaseService {
+}
---
+++ christ-web/src/main/java/org/youngguang/app/action/BaseController.java
@@ -0,0 +1,7 @@
+package org.youngguang.app.action;
+
+/**
+ * Created by Administrator on 2014/5/6 0006.
+ */
+public class BaseController {
+}
